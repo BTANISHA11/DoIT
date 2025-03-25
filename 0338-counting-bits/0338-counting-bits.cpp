587b771fcd3a824921212fbddb19bbabb3496a27
TANISHA BANSAL <110708865+BTANISHA11@users.noreply.github.com>
2025-03-25 23:21:16 +0530

    Time: 0 ms (100%), Space: 10.6 MB (80.24%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,10 +1,32 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> arr(n+1,0);
-        for(int i=1;i<=n;++i){
-            arr[i]=__builtin_popcount(i);
+//         vector<int> arr(n+1,0);
+//         for(int i=1;i<=n;++i){
+//             arr[i]=__builtin_popcount(i);
+//         }
+//         return arr;
+//     }
+// };
+
+ vector<int> result(n+1);
+        
+        if(n == 0)
+            return result;
+        
+        result[1] = 1;
+        
+        for(int i = 2; i < n+1; i++) {
+            
+            if(i%2 == 0) {
+                result[i] = result[i/2];
+            } else {
+                result[i] = result[i/2] + 1;
+            }
+            
         }
-        return arr;
+        
+        return result;
+        
     }
 };
