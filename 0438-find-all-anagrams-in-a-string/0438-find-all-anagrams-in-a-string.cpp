cb1d0c8b06dcee469ade8d658c9473492e835684
TANISHA BANSAL <110708865+BTANISHA11@users.noreply.github.com>
2025-03-27 00:32:32 +0530

    Time: 7 ms (53.57%), Space: 11.6 MB (66.11%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> result;
+        if(s.size() < p.size()) return result;
+
+        vector<int> pCount(26, 0), sCount(26, 0);
+        for(char c : p) {
+            pCount[c - 'a']++;
+        }
+
+        // Initial window
+        for(int i = 0; i < p.size(); ++i) {
+            sCount[s[i] - 'a']++;
+        }
+
+        if(sCount == pCount) result.push_back(0);
+
+        for(int i = p.size(); i < s.size(); ++i) {
+            // Sliding the window forward
+            sCount[s[i] - 'a']++;
+            sCount[s[i - p.size()] - 'a']--;
+
+            if(sCount == pCount) result.push_back(i - p.size() + 1);
+        }
+        
+        return result;
+    }
+};
