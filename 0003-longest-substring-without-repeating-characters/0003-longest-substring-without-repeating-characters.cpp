e04f5ca9d5bb278d82a70d235d5e48c50d476775
TANISHA BANSAL <110708865+BTANISHA11@users.noreply.github.com>
2025-03-27 12:36:31 +0530

    Time: 0 ms (100%), Space: 11.6 MB (82.06%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int ans=0,l=0,r=0;
-        int  n =s.size();
-        vector<int> map(256,-1);
-        while(r<n){
-            if(map[s[r]]!=-1){
-                l=max(map[s[r]]+1,l);
-            }
-            map[s[r]]=r;
-            ans= max(ans,r-l+1);
-            r++;
+    int maxlen=0,l=0,r=0;
+    vector<int> map(256,-1);
+    int n=s.size();
+    while(r<n){
+        if(map[s[r]]!=-1) { //in map if char at ind r seen before
+        if(map[s[r]]>=l){
+            // If it has been seen and its last recorded index (map[s[r]]) is greater than or equal to l, it means the character is within the current substring defined by the window [l, r]. Therefore, we need to move the left pointer l to the right of the last occurrence of the character to avoid repeating it.
+        l=map[s[r]]+1; //in range
         }
-        return ans;
+        }
+int len=r-l+1;
+maxlen=max(maxlen,len);
+map[s[r]]=r; //This updates the last seen index of the character s[r] to the current index r.
+r++;
+    }
+    return maxlen;
     }
 };
