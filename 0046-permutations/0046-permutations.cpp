5c0ad6c2ceecf2dc3f81b2e0c761bcc1cfb9cbd0
TANISHA BANSAL <110708865+BTANISHA11@users.noreply.github.com>
2025-04-07 11:27:31 +0530

    Time: 0 ms (100%), Space: 10.6 MB (70.28%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-void backtrack(vector<int>& nums,vector<vector<int>>& res,int start){
-    if(start==nums.size()){
-    res.push_back(nums);
-    return ;
-    }
-    for(int i=start;i<nums.size();i++){
-        swap(nums[start],nums[i]);
-        backtrack(nums,res,start+1);
-        swap(nums[start],nums[i]);
-    }
+// void backtrack(vector<int>& nums,vector<vector<int>>& res,int start){
+//     if(start==nums.size()){
+//     res.push_back(nums);
+//     return ;
+//     }
+//     for(int i=start;i<nums.size();i++){
+//         swap(nums[start],nums[i]);
+//         backtrack(nums,res,start+1);
+//         swap(nums[start],nums[i]);
+//     }
 
-}
+// }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> res;
-        backtrack(nums,res,0);
-        return res;
+        // vector<vector<int>> res;
+        // backtrack(nums,res,0);
+        // return res;
+         vector<vector<int>> result;
+        // Sort the numbers to start with the smallest lexicographical permutation
+        sort(nums.begin(), nums.end());
+        do {
+            // Add the current permutation to the list
+            result.push_back(nums);
+            // Generate the next permutation
+        } while(next_permutation(nums.begin(), nums.end()));
+        return result;
     }
 };
